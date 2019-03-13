70662f64a96a75b142716ac80f743daa3991656a
PetteriM1 <petteri.malkavaara1@gmail.com>
2019-03-13 17:45:48 +0200

    NukkitX support

--- src/main/java/org/itxtech/synapseapi/SynapsePlayer.java
+++ src/main/java/org/itxtech/synapseapi/SynapsePlayer.java
@@ -16,6 +16,7 @@ import cn.nukkit.math.NukkitMath;
 import cn.nukkit.nbt.tag.*;
 import cn.nukkit.network.SourceInterface;
 import cn.nukkit.network.protocol.*;
+import cn.nukkit.utils.MainLogger;
 import cn.nukkit.utils.TextFormat;
 import org.itxtech.synapseapi.event.player.SynapsePlayerConnectEvent;
 import org.itxtech.synapseapi.event.player.SynapsePlayerTransferEvent;
@@ -25,6 +26,9 @@ import org.itxtech.synapseapi.utils.ClientData;
 import org.itxtech.synapseapi.utils.ClientData.Entry;
 import org.itxtech.synapseapi.utils.DataPacketEidReplacer;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 /**
@@ -32,9 +36,20 @@ import java.util.*;
  */
 public class SynapsePlayer extends Player {
 
+    private static final Method updateName;
+
     public boolean isSynapseLogin = false;
     protected SynapseEntry synapseEntry;
     private boolean isFirstTimeLogin = false;
+
+    static {
+        try {
+            updateName = Server.class.getMethod("updateName", UUID.class, String.class);
+            updateName.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public SynapsePlayer(SourceInterface interfaz, SynapseEntry synapseEntry, Long clientID, String ip, int port) {
         super(interfaz, clientID, ip, port);
@@ -109,11 +124,28 @@ public class SynapsePlayer extends Player {
             }
         }
 
-        CompoundTag nbt = this.server.getOfflinePlayerData(this.username);
+        CompoundTag nbt;
+        File legacyDataFile = new File(server.getDataPath() + "players/" + this.username.toLowerCase() + ".dat");
+        File dataFile = new File(server.getDataPath() + "players/" + this.uuid.toString() + ".dat");
+        if (legacyDataFile.exists() && !dataFile.exists()) {
+            nbt = this.server.getOfflinePlayerData(this.username);
+
+            if (!legacyDataFile.delete()) {
+                MainLogger.getLogger().warning("Could not delete legacy player data for " + this.username);
+            }
+        } else {
+            nbt = this.server.getOfflinePlayerData(this.uuid);
+        }
+
         if (nbt == null) {
             this.close(this.getLeaveMessage(), "Invalid data!");
-
             return;
+        }
+
+        if (getLoginChainData().isXboxAuthed() && server.getPropertyBoolean("xbox-auth") || !server.getPropertyBoolean("xbox-auth")) {
+            try {
+                updateName.invoke(server, this.uuid, this.username);
+            } catch (IllegalAccessException | InvocationTargetException e) {}
         }
 
         this.playedBefore = (nbt.getLong("lastPlayed") - nbt.getLong("firstPlayed")) > 1;
@@ -168,7 +200,7 @@ public class SynapsePlayer extends Player {
         nbt.putLong("lastPlayed", System.currentTimeMillis() / 1000);
 
         if (this.server.getAutoSave()) {
-            this.server.saveOfflinePlayerData(this.username, nbt, true);
+            this.server.saveOfflinePlayerData(this.uuid, nbt, true);
         }
 
         this.sendPlayStatus(PlayStatusPacket.LOGIN_SUCCESS);
@@ -250,12 +282,6 @@ public class SynapsePlayer extends Player {
             startGamePacket.commandsEnabled = this.isEnableClientCommand();
             startGamePacket.worldName = this.getServer().getNetwork().getName();
             startGamePacket.gameRules = this.getLevel().getGameRules();
-
-            try {
-                Class.forName("cn.nukkit.utils.EntityUtils");
-                startGamePacket.protocol = this.protocol;
-            } catch (Exception ignore) {}
-
             this.dataPacket(startGamePacket);
         } else {
             AdventureSettings newSettings = this.getAdventureSettings().clone(this);
@@ -309,11 +335,9 @@ public class SynapsePlayer extends Player {
 
         try {
             Class.forName("cn.nukkit.utils.EntityUtils");
-            if (this.protocol >= 313) {
-                this.getServer().getScheduler().scheduleTask(null, () -> {
-                    this.dataPacket(new AvailableEntityIdentifiersPacket());
-                }, true);
-            }
+            this.getServer().getScheduler().scheduleTask(null, () -> {
+                this.dataPacket(new AvailableEntityIdentifiersPacket());
+            }, true);
         } catch (Exception ignore) {}
 
         this.server.addOnlinePlayer(this);
